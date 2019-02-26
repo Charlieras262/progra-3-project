53b6a34346be563ba080af59999977a611f98cf4
Carlos <charlieras262@gmail.com>
2019-02-26 16:45:36 -0600

    get, post, put and delete requests added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,18 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/user.controller');
 
-//Peticionnes http al servidor
-router.get('/', controller.getUser)
+/* Peticionnes http al servidor */
+
+// Obtener Usuarios
+router.get('/', controller.getUsers)
+// Obtener Usuario por id
+router.get('/:id', controller.getUser)
+// Crear Usuario
+router.post('/', controller.createUser);
+// Actualizar Usuario
+router.put('/:id', controller.updateUser);
+// Eliminar Usuario
+router.delete('/:id', controller.deleteUser);
 
 // Se exporta el rauter para poder utilizar sus rutas e indexar 
 module.exports = router
