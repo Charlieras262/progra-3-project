7507e2b6632197b777d4510e469c3687194b334a
Carlos <charlieras262@gmail.com>
2019-02-26 16:44:50 -0600

    get, create, update and delete options added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,14 +2,72 @@ const User = require('../models/User')
 const UserController = {}
 
 /**
- * @argument req es el requerimiento que envia el front end al servidor
- * @argument res es la respuesta que envia el servidor al front end
+ * @argument req es el requerimiento que envia el front end al servidor.
+ * @argument res es la respuesta que envia el servidor al front end.
+ * @returns un json con un estado, un mensaje y un arreglo de usuarios si existen.
+ */
+UserController.getUsers = async(req, res) => {
+    const user = await User.find()
+    if(user.length === 0) res.json({
+        status: false, 
+        msg: 'Users not found'
+    })
+    else res.json({
+        status: true, 
+        msg:'Users found', 
+        user})
+}
+
+/**
+ * @argument req es el requerimiento que envia el front end al servidor.
+ * @argument res es la respuesta que envia el servidor al front end.
+ * @returns un json con un estado, un mensaje y un usuario si existe.
  */
 UserController.getUser = async(req, res) => {
-    const user = await User.find()
-    if(user.length === 0) res.json({status: 'Error', msg: 'User not found'})
-    else res.json({status: 'Success', msg:'Users found', user})
-    
+    const user = await User.findById(req.params.id)
+    if(user.length === 0) res.json({status: false, msg: 'User not found'})
+    else res.json({status: true, msg:'User found', user})
+}
+
+
+/**
+ * @argument req es el requerimiento que envia el front end al servidor.
+ * @argument res es la respuesta que envia el servidor al front end.
+ * @returns un json con un estado, un mensaje.
+ */
+UserController.createUser = async(req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.json({
+        status: true,
+        msg: 'User created'
+    });
+}
+
+/**
+ * @argument req es el requerimiento que envia el front end al servidor.
+ * @argument res es la respuesta que envia el servidor al front end.
+ * @returns un json con un estado, un mensaje.
+ */
+UserController.updateUser = async(req, res) => {
+    await User.findByIdAndUpdate(req.params.id, req.body)
+    res.json({
+        status: true, 
+        msg:'User updated'
+    })
+}
+
+/**
+ * @argument req es el requerimiento que envia el front end al servidor.
+ * @argument res es la respuesta que envia el servidor al front end.
+ * @returns un json con un estado, un mensaje y un usuario si existe.
+ */
+UserController.deleteUser = async(req, res) => {
+    await User.findByIdAndDelete(req.params.id)
+    res.json({
+        status: true, 
+        msg:'User found'
+    })
 }
 
 //Se exporta el controllador para poder utilizarlo en las rutas
