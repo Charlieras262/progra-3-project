b5f6c8882410936a010398523429a068b275e0bd
Carlos <charlieras262@gmail.com>
2019-02-27 02:28:59 -0600

    updated

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
 
 // En este esquema se establece el modelo del Usuario
 const UserSchema = new Schema({
-    name: {type: String, required: true},
-    lastname: {type: String, required: true},
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    type: {type: String, required: true}
-})
+  name: {type: String, required: true},
+  lastname: {type: String, required: true},
+  username: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  type: {type: String, required: true}});
 
-//Se exporta el esquema como un modelo para utilizarlo en el controllador
-module.exports = mongoose.model('User', UserSchema)
+// Se exporta el esquema como un modelo para utilizarlo en el controllador
+module.exports = mongoose.model('User', UserSchema);
