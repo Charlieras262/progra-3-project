18111af30b80838e1d27499bc668710a6505fba1
Carlos <charlieras262@gmail.com>
2019-02-27 02:27:48 -0600

    empty field validation added and jsdoc updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,64 +1,85 @@
-const User = require('../models/User')
-const UserController = {}
+const User = require('../models/User');
+const generalValidations = require('./validations/general.validation');
+const UserController = {};
 
 /**
  * Este metodo obtiene todos los usuarios de la base de datos
- * @argument req es el requerimiento que envia el front end al servidor.
- * @argument res es la respuesta que envia el servidor al front end.
- * @returns un json con un estado, un mensaje y un arreglo de usuarios si existen.
+ * @param {*} req es el requerimiento que envia el front end al servidor.
+ * @param {*} res es la respuesta que envia el servidor al front end.
+ * @return {JSON} un json con un estado, un mensaje
+ * y un arreglo de usuarios si existen.
  */
-UserController.getUsers = async(req, res) => {
-    const user = await User.find()
-    if(user.length === 0) res.json({status: false, msg: 'Users not found'})
-    else res.json({status: true, msg:'Users found', user})
-}
+UserController.getUsers = async (req, res) => {
+  const user = await User.find();
+  if (user.length === 0) res.json({status: false, msg: 'Users not found'});
+  else res.json({status: true, msg: 'Users found', user});
+};
 
 /**
  * Este metodo obtiene un usuario filtrado por id
- * @argument req es el requerimiento que envia el front end al servidor.
- * @argument res es la respuesta que envia el servidor al front end.
- * @returns un json con un estado, un mensaje y un usuario si existe.
+ * @param {*} req es el requerimiento que envia el front end al servidor.
+ * @param {*} res es la respuesta que envia el servidor al front end.
+ * @return {JSON} un json con un estado, un mensaje y un usuario si existe.
  */
-UserController.getUser = async(req, res) => {
-    const user = await User.findById(req.params.id)
-    if(user.length === 0) res.json({status: false, msg: 'User not found'})
-    else res.json({status: true, msg:'User found', user})
-}
+UserController.getUser = async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+  if (!user) res.json({status: false, msg: 'User not found'});
+  else res.json({status: true, msg: 'User found', user});
+};
 
 
 /**
  * Este medotodo guarda un usuario en la base de datos
- * @argument req es el requerimiento que envia el front end al servidor.
- * @argument res es la respuesta que envia el servidor al front end.
- * @returns un json con un estado, un mensaje.
+ * @param {*} req es el requerimiento que envia el front end al servidor.
+ * @param {*} res es la respuesta que envia el servidor al front end.
+ * @return {JSON} un json con un estado, un mensaje.
  */
-UserController.createUser = async(req, res) => {
-    const user = new User(req.body);
+UserController.createUser = async (req, res) => {
+  const user = new User(req.body);
+  const authRes = authUserInfo(user);
+  if (authRes.success) {
     await user.save();
     res.json({status: true, msg: 'User created'});
-}
+  } else {
+    res.json(authRes);
+  }
+};
 
 /**
  * Este metodo actualiza la informacion de un usuario en la base de datos
- * @argument req es el requerimiento que envia el front end al servidor.
- * @argument res es la respuesta que envia el servidor al front end.
- * @returns un json con un estado, un mensaje.
+ * @param {*} req es el requerimiento que envia el front end al servidor.
+ * @param {*} res es la respuesta que envia el servidor al front end.
+ * @return {JSON} un json con un estado, un mensaje.
  */
-UserController.updateUser = async(req, res) => {
-    await User.findByIdAndUpdate(req.params.id, req.body)
-    res.json({status: true, msg:'User updated'})
-}
+UserController.updateUser = async (req, res) => {
+  await User.findByIdAndUpdate(req.params.id, req.body);
+  res.json({status: true, msg: 'User updated'});
+};
 
 /**
  * Este metodo elimina a un usuario de la base de datos
- * @argument req es el requerimiento que envia el front end al servidor.
- * @argument res es la respuesta que envia el servidor al front end.
- * @returns un json con un estado, un mensaje y un usuario si existe.
+ * @param {*} req es el requerimiento que envia el front end al servidor.
+ * @param {*} res es la respuesta que envia el servidor al front end.
+ * @return {JSON} un json con un estado, un mensaje y un usuario si existe.
  */
-UserController.deleteUser = async(req, res) => {
-    await User.findByIdAndDelete(req.params.id)
-    res.json({status: true, msg:'User deleted'})
-}
+UserController.deleteUser = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({status: true, msg: 'User deleted'});
+};
 
-//Se exporta el controllador para poder utilizarlo en las rutas
-module.exports = UserController
+const authUserInfo = (user) => {
+  const names = ['Name', 'Last Name', 'Username', 'Email', 'Password'];
+  // Se convierte un objeto http a string,
+  // luego a JSON y por ultimo a un arreglo
+  user = Object.values(JSON.parse(JSON.stringify(user)));
+  let res;
+  for (let i = 1; i < (user.length); i++) {
+    res = generalValidations.isFilled(user[i], names[i-1]);
+    if (!res.success) return res;
+  }
+  return {success: true, msg: 'Everything is clear'};
+};
+
+// Se exporta el controllador para poder utilizarlo en las rutas
+module.exports = UserController;
