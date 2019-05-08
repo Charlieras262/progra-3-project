7280f7bcce549bcd0a5b46f6caa2ddc8c08942b5
Carlos <charlyras262@gmail.com>
2019-05-07 20:58:47 -0600

    navbar, login, singup and home components added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardProfComponent } from './components/dashboard-prof/dashboard-prof.component';
+import { DashboardAdminComponent } from './components/dashboard-admin/dashboard-admin.component';
+import { DashboardEstComponent } from './components/dashboard-est/dashboard-est.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingupComponent } from './components/singup/singup.component';
+import { FormsModule } from '@angular/forms';
+//Translation
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    DashboardProfComponent,
+    DashboardAdminComponent,
+    DashboardEstComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [ HttpClient ]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(public translateService: TranslateService) {
+    this.translateService.setDefaultLang('es');
+   }
+
+  ngOnInit() {
+  }
+
+  onLogoutClick(){
+    this.translateService.use('en')
+    console.log(this.translateService.get('home').subscribe());
+  }
+
+}
