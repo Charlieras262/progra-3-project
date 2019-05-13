bff43fdda4798e08262a6ec2547dc450aa2f2dbc
Carlos <charlyras262@gmail.com>
2019-05-13 14:03:47 -0600

    getStudentsAmount loadToken and loadUser methods changed
    El metodo getStudentesAmount returna una cantidad de estudiantes que esten registrados en la plataforma.

--- client/src/app/services/estudiantes/estudiantes.service.ts
+++ client/src/app/services/estudiantes/estudiantes.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ProjectVariable } from 'src/app/variables/projects.variables';
+import { AuthenticateService } from '../authenticate/authenticate.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudiantesService {
-readonly API_URL = ProjectVariable.serverLocation + 'api/students';
-  constructor(public http: HttpClient) {}
-  
-  obtenerUsuario(){
- let headers = new HttpHeaders().set('Content-Type', 'application/json');
- return this.http.get(this.API_URL,  { headers: headers });
- 
+
+  readonly API_URL = ProjectVariable.serverLocation + 'api/students';
+  authToken: any;
+  user: any;
+
+  constructor(public http: HttpClient,
+    public authService: AuthenticateService) { }
+
+  getStudentsAmount() {
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.get(`${this.API_URL}/amount`, { headers: headers });
   }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loadUser() {
+    const user = localStorage.getItem('user');
+    this.user = user;
+  }
+
 }
