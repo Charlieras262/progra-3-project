1583ccbb5295550df69294a57908f63213c20290
Carlos <charlyras262@gmail.com>
2019-05-13 14:07:13 -0600

    amount route was added

--- server/src/routes/students.routes.js
+++ server/src/routes/students.routes.js
@@ -4,6 +4,7 @@ const passport = require('passport')
 
 const student = require('../controllers/student.controller');
 
+router.get('/amount', student.getStudentsAmount);
 router.get('/', passport.authenticate('jwt', { session: false }), student.getStudents);
 router.get('/:id', passport.authenticate('jwt', { session: false }), student.getStudent);
 router.post('/', passport.authenticate('jwt', { session: false }), student.createStudent);
